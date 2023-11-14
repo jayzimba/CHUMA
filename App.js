72782a68ede9ff2fec6530f4b1039147fba1d985
Geoffrey zimba <jayzimba40@gmail.com>
2023-11-14 08:20:29 +0200

    chuma cloned

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const url = "https://jimpl.com/"; // Replace with the URL of your website
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView source={{ uri: url }} />
+      <View
+        style={{
+          backgroundColor: "#21dc96",
+          height: 200,
+          width: "100%",
+          position: "absolute",
+          paddingTop: 50,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 32, fontWeight: "500", color: "white" }}>
+          Secure your data today
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "500", color: "white" }}>
+          PONCHO DETECTIVE SOFTWARE
+        </Text>
+      </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
